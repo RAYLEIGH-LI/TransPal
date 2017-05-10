1c4f60afce5230a07932b93c5d53fa513ad39801
njxiaohan <njxiaohan@qq.com>
2017-05-10 09:36:38 +0800

    newly build

--- ios/build/Build/Intermediates/RCTNetwork.build/Debug-iphonesimulator/RCTNetwork.build/Objects-normal/x86_64/RCTDataRequestHandler.d
+++ ios/build/Build/Intermediates/RCTNetwork.build/Debug-iphonesimulator/RCTNetwork.build/Objects-normal/x86_64/RCTDataRequestHandler.d
@@ -1,8 +1,4 @@
 dependencies: \
-  /Users/njxiaohan/react-native/app/node_modules/react-native/Libraries/Network/RCTDataRequestHandler.m \
-  /Users/njxiaohan/react-native/app/node_modules/react-native/Libraries/Network/RCTDataRequestHandler.h \
-  /Users/njxiaohan/react-native/app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTInvalidating.h \
-  /Users/njxiaohan/react-native/app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTURLRequestHandler.h \
-  /Users/njxiaohan/react-native/app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeModule.h \
-  /Users/njxiaohan/react-native/app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTDefines.h \
-  /Users/njxiaohan/react-native/app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTURLRequestDelegate.h
+  /Users/njxiaohan/react-native/TransPal/node_modules/react-native/Libraries/Network/RCTDataRequestHandler.m \
+  /Users/njxiaohan/react-native/TransPal/node_modules/react-native/Libraries/Network/RCTDataRequestHandler.h \
+  /Users/njxiaohan/react-native/TransPal/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTInvalidating.h
